49aeb1f55782a984e1da412dcfb7277723bfb4c0
Bogdan Bodnar <bogdan.bodnar@nic.cz>
2020-01-08 14:44:15 +0100

    Fix python tests with new backend mock.

--- tests/test_authority.py
+++ tests/test_authority.py
@@ -1,50 +1,68 @@
+#  Copyright (C) 2020 CZ.NIC z.s.p.o. (http://www.nic.cz/)
+#
+#  This is free software, licensed under the GNU General Public License v3.
+#  See /LICENSE for more information.
+
 from http import HTTPStatus
 
-from .utils import get_mocked_remote_access_client
-
+from reforis.test_utils import mock_backend_response
 
 AUTHORITY_URL = '/remote-access/api/authority'
 
 
-def test_get_authority(app):
-    backend_response = {'status': 'missing', 'something_else': 'duh!'}
-    with get_mocked_remote_access_client(app, backend_response) as client:
-        response = client.get(AUTHORITY_URL)
+@mock_backend_response({
+    'remote': {
+        'get_status': {
+            'status': 'missing',
+            'something_else': 'duh!'
+        }
+    }
+})
+def test_get_authority(client):
+    response = client.get(AUTHORITY_URL)
     assert response.status_code == HTTPStatus.OK
-    assert response.json == {'status': backend_response['status']}
+    assert response.json == {'status': 'missing'}
 
 
-def test_post_authority(app):
-    backend_response = {'status': 'whatever'}
-    with get_mocked_remote_access_client(app, backend_response) as client:
-        response = client.post(AUTHORITY_URL)
+@mock_backend_response({
+    'remote': {
+        'get_status': {'status': 'whatever'},
+        'generate_ca': {'authority': 'whatever'}
+    }
+})
+def test_post_authority(client):
+    response = client.post(AUTHORITY_URL)
     assert response.status_code == HTTPStatus.ACCEPTED
-    assert response.json == backend_response
+    assert response.json == {'authority': 'whatever'}
 
 
-def test_post_authority_already_exists(app):
-    with get_mocked_remote_access_client(app, {'status': 'ready'}) as client:
-        response = client.post(AUTHORITY_URL)
+@mock_backend_response({
+    'remote': {
+        'get_status': {'status': 'ready'},
+    }
+})
+def test_post_authority_already_exists(client):
+    response = client.post(AUTHORITY_URL)
     assert response.status_code == HTTPStatus.BAD_REQUEST
     assert response.json == 'Certificate authority already exists.'
 
 
-def test_delete_authority(app):
-    with get_mocked_remote_access_client(app, {'result': True}) as client:
-        response = client.delete(AUTHORITY_URL)
+@mock_backend_response({'remote': {'delete_ca': {'result': True}, }})
+def test_delete_authority(client):
+    response = client.delete(AUTHORITY_URL)
     assert response.status_code == HTTPStatus.NO_CONTENT
     assert response.data == b''
 
 
-def test_delete_authority_backend_error_no_result(app):
-    with get_mocked_remote_access_client(app, {}) as client:
-        response = client.delete(AUTHORITY_URL)
+@mock_backend_response({'remote': {'delete_ca': {}, }})
+def test_delete_authority_backend_error_no_result(client):
+    response = client.delete(AUTHORITY_URL)
     assert response.status_code == HTTPStatus.INTERNAL_SERVER_ERROR
     assert response.json == 'Cannot delete certificate authority.'
 
 
-def test_delete_authority_backend_error_wrong_result(app):
-    with get_mocked_remote_access_client(app, {'result': 1234}) as client:
-        response = client.delete(AUTHORITY_URL)
+@mock_backend_response({'remote': {'delete_ca': {'result': 1234}, }})
+def test_delete_authority_backend_error_wrong_result(client):
+    response = client.delete(AUTHORITY_URL)
     assert response.status_code == HTTPStatus.INTERNAL_SERVER_ERROR
     assert response.json == 'Cannot delete certificate authority.'
