0db877f3c50df05fce9216b971b4035da9c0ca6b
Bogdan Bodnar <bogdan.bodnar@nic.cz>
2020-03-30 08:56:22 +0200

    Bump v1.2.0.

--- setup.py
+++ setup.py
@@ -32,7 +32,7 @@ class RemoteAccessBuild(build_py):
 
 setuptools.setup(
     name=NAME,
-    version='1.1.1',
+    version='1.2.0',
     packages=setuptools.find_packages(exclude=['tests']),
     include_package_data=True,
 
