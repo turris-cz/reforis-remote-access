5877355d291caa97d66435253a7c24dc621351f5
Aleksandr Gumroian <aleksandr.gumroian@nic.cz>
2021-03-17 15:23:07 +0100

    setup.py: Remove foris-client & paho-mqtt from extras
    As reForis already has foris-client & paho-mqtt in install
    dependencies, we can get rid of them in plugins extras.

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-#  Copyright (C) 2019 CZ.NIC z.s.p.o. (http://www.nic.cz/)
+#  Copyright (C) 2019-2021 CZ.NIC z.s.p.o. (http://www.nic.cz/)
 #
 #  This is free software, licensed under the GNU General Public License v3.
 #  See /LICENSE for more information.
@@ -52,8 +52,6 @@ setuptools.setup(
             'pylint-quotes',
             'pycodestyle',
             'reforis @ git+https://gitlab.nic.cz/turris/reforis/reforis#egg=reforis',
-            'foris-client @ git+https://gitlab.nic.cz/turris/foris-controller/foris-client#egg=foris-client',
-            'paho-mqtt',
         ],
     },
     setup_requires=[
