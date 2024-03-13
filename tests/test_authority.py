811694112a00c2adcb72314afae291ca0e9ffb1e
Aleksandr Gumroian <aleksandr.gumroian@nic.cz>
2024-03-13 12:07:00 +0100

    Format Python files with Ruff

--- tests/test_authority.py
+++ tests/test_authority.py
@@ -1,4 +1,4 @@
-#  Copyright (C) 2020 CZ.NIC z.s.p.o. (http://www.nic.cz/)
+#  Copyright (C) 2020-2024 CZ.NIC z.s.p.o. (https://www.nic.cz/)
 #
 #  This is free software, licensed under the GNU General Public License v3.
 #  See /LICENSE for more information.
@@ -7,62 +7,70 @@ from http import HTTPStatus
 
 from reforis.test_utils import mock_backend_response
 
-AUTHORITY_URL = '/remote-access/api/authority'
+AUTHORITY_URL = "/remote-access/api/authority"
 
 
-@mock_backend_response({
-    'remote': {
-        'get_status': {
-            'status': 'missing',
-            'something_else': 'duh!'
-        }
-    }
-})
+@mock_backend_response({"remote": {"get_status": {"status": "missing", "something_else": "duh!"}}})
 def test_get_authority(client):
     response = client.get(AUTHORITY_URL)
     assert response.status_code == HTTPStatus.OK
-    assert response.json == {'status': 'missing'}
+    assert response.json == {"status": "missing"}
 
 
-@mock_backend_response({
-    'remote': {
-        'get_status': {'status': 'whatever'},
-        'generate_ca': {'authority': 'whatever'}
-    }
-})
+@mock_backend_response({"remote": {"get_status": {"status": "whatever"}, "generate_ca": {"authority": "whatever"}}})
 def test_post_authority(client):
     response = client.post(AUTHORITY_URL)
     assert response.status_code == HTTPStatus.ACCEPTED
-    assert response.json == {'authority': 'whatever'}
+    assert response.json == {"authority": "whatever"}
 
 
-@mock_backend_response({
-    'remote': {
-        'get_status': {'status': 'ready'},
+@mock_backend_response(
+    {
+        "remote": {
+            "get_status": {"status": "ready"},
+        }
     }
-})
+)
 def test_post_authority_already_exists(client):
     response = client.post(AUTHORITY_URL)
     assert response.status_code == HTTPStatus.BAD_REQUEST
-    assert response.json == 'Certificate authority already exists.'
+    assert response.json == "Certificate authority already exists."
 
 
-@mock_backend_response({'remote': {'delete_ca': {'result': True}, }})
+@mock_backend_response(
+    {
+        "remote": {
+            "delete_ca": {"result": True},
+        }
+    }
+)
 def test_delete_authority(client):
     response = client.delete(AUTHORITY_URL)
     assert response.status_code == HTTPStatus.NO_CONTENT
-    assert response.data == b''
+    assert response.data == b""
 
 
-@mock_backend_response({'remote': {'delete_ca': {}, }})
+@mock_backend_response(
+    {
+        "remote": {
+            "delete_ca": {},
+        }
+    }
+)
 def test_delete_authority_backend_error_no_result(client):
     response = client.delete(AUTHORITY_URL)
     assert response.status_code == HTTPStatus.INTERNAL_SERVER_ERROR
-    assert response.json == 'Cannot delete certificate authority.'
+    assert response.json == "Cannot delete certificate authority."
 
 
-@mock_backend_response({'remote': {'delete_ca': {'result': 1234}, }})
+@mock_backend_response(
+    {
+        "remote": {
+            "delete_ca": {"result": 1234},
+        }
+    }
+)
 def test_delete_authority_backend_error_wrong_result(client):
     response = client.delete(AUTHORITY_URL)
     assert response.status_code == HTTPStatus.INTERNAL_SERVER_ERROR
-    assert response.json == 'Cannot delete certificate authority.'
+    assert response.json == "Cannot delete certificate authority."
--- tests/test_tokens.py
+++ tests/test_tokens.py
@@ -1,113 +1,112 @@
+#  Copyright (C) 2020-2024 CZ.NIC z.s.p.o. (https://www.nic.cz/)
+#
+#  This is free software, licensed under the GNU General Public License v3.
+#  See /LICENSE for more information.
+
 from http import HTTPStatus
 
 from reforis.test_utils import mock_backend_response
 
-TOKENS_URL = '/remote-access/api/tokens'
-EXAMPLE_TOKEN_URL = f'{TOKENS_URL}/1234'
+TOKENS_URL = "/remote-access/api/tokens"
+EXAMPLE_TOKEN_URL = f"{TOKENS_URL}/1234"
 
 
-@mock_backend_response({'remote': {'get_status': {'tokens': []}}})
+@mock_backend_response({"remote": {"get_status": {"tokens": []}}})
 def test_get_tokens(client):
     response = client.get(TOKENS_URL)
     assert response.status_code == HTTPStatus.OK
     assert response.json == []
 
 
-@mock_backend_response({
-    'remote': {
-        'get_status': {'tokens': []},
-        'generate_token': {'task_id': 1234}
-    }
-})
+@mock_backend_response({"remote": {"get_status": {"tokens": []}, "generate_token": {"task_id": 1234}}})
 def test_post_token(client):
-    response = client.post(TOKENS_URL, json={'name': 'Joe Sixpack'})
+    response = client.post(TOKENS_URL, json={"name": "Joe Sixpack"})
     assert response.status_code == HTTPStatus.ACCEPTED
-    assert response.json == {'task_id': 1234}
+    assert response.json == {"task_id": 1234}
 
 
-@mock_backend_response({
-    'remote': {
-        'get_status': {'tokens': [{'name': 'Joe Sixpack'}]},
+@mock_backend_response(
+    {
+        "remote": {
+            "get_status": {"tokens": [{"name": "Joe Sixpack"}]},
+        }
     }
-})
+)
 def test_post_token_duplicate(client):
-    response = client.post(TOKENS_URL, json={'name': 'Joe Sixpack'})
+    response = client.post(TOKENS_URL, json={"name": "Joe Sixpack"})
     assert response.status_code == HTTPStatus.CONFLICT
-    assert response.json == 'Token \'Joe Sixpack\' already exists.'
+    assert response.json == "Token 'Joe Sixpack' already exists."
 
 
 def test_post_token_missing_field(client):
-    response = client.post(TOKENS_URL, json={'codename': 'Duchess'})
+    response = client.post(TOKENS_URL, json={"codename": "Duchess"})
     assert response.status_code == HTTPStatus.BAD_REQUEST
-    assert response.json == {'name': 'Missing data for required field.'}
+    assert response.json == {"name": "Missing data for required field."}
 
 
 def test_post_token_invalid_json(client):
     response = client.post(TOKENS_URL)
     assert response.status_code == HTTPStatus.BAD_REQUEST
-    assert response.json == 'Invalid JSON'
+    assert response.json == "Invalid JSON"
 
 
 def test_post_token_invalid_data(client):
-    response = client.post(TOKENS_URL, json={'name': 1234})
+    response = client.post(TOKENS_URL, json={"name": 1234})
     assert response.status_code == HTTPStatus.BAD_REQUEST
-    assert response.json == {'name': 'Expected data of type: str'}
+    assert response.json == {"name": "Expected data of type: str"}
 
 
-@mock_backend_response({
-    'remote': {
-        'get_status': {'tokens': []},
-        'generate_token': {}
-    }
-})
+@mock_backend_response({"remote": {"get_status": {"tokens": []}, "generate_token": {}}})
 def test_post_token_backend_error(client):
-    response = client.post(TOKENS_URL, json={'name': 'Erika Mustermann'})
+    response = client.post(TOKENS_URL, json={"name": "Erika Mustermann"})
     assert response.status_code == HTTPStatus.INTERNAL_SERVER_ERROR
-    assert response.json == 'Cannot create token.'
+    assert response.json == "Cannot create token."
 
 
-@mock_backend_response({
-    'remote': {
-        'get_token': {'token': 'FOO=BAR'},
-        'get_status': {'tokens': [{'id': '1234', 'name': 'foobar'}]},
+@mock_backend_response(
+    {
+        "remote": {
+            "get_token": {"token": "FOO=BAR"},
+            "get_status": {"tokens": [{"id": "1234", "name": "foobar"}]},
+        }
     }
-})
+)
 def test_get_token(client):
-    response = client.get(f'{TOKENS_URL}/1234')
+    response = client.get(f"{TOKENS_URL}/1234")
     assert response.status_code == HTTPStatus.OK
-    assert response.headers.get('Content-Disposition') == f'attachment; filename=token-foobar.tar.gz'
-    assert response.data == b'\x14\xe3'
+    assert response.headers.get("Content-Disposition") == f"attachment; filename=token-foobar.tar.gz"
+    assert response.data == b"\x14\xe3"
 
 
-@mock_backend_response({'remote': {'get_token': {'status': 'not_found'}}})
+@mock_backend_response({"remote": {"get_token": {"status": "not_found"}}})
 def test_get_token_not_found(client):
     response = client.get(EXAMPLE_TOKEN_URL)
     assert response.status_code == HTTPStatus.NOT_FOUND
-    assert response.json == 'Requested token does not exist.'
+    assert response.json == "Requested token does not exist."
 
 
-@mock_backend_response({'remote': {'get_token': {}}})
+@mock_backend_response({"remote": {"get_token": {}}})
 def test_get_token_backend_error(client):
     response = client.get(EXAMPLE_TOKEN_URL)
     assert response.status_code == HTTPStatus.INTERNAL_SERVER_ERROR
-    assert response.json == 'Cannot get token.'
+    assert response.json == "Cannot get token."
 
 
-@mock_backend_response({'remote': {'revoke': {'result': True}}})
+@mock_backend_response({"remote": {"revoke": {"result": True}}})
 def test_delete_client(client):
     response = client.delete(EXAMPLE_TOKEN_URL)
     assert response.status_code == HTTPStatus.NO_CONTENT
 
 
-@mock_backend_response({'remote': {'revoke': {}}})
+@mock_backend_response({"remote": {"revoke": {}}})
 def test_delete_client_backend_error(client):
     response = client.delete(EXAMPLE_TOKEN_URL)
     assert response.status_code == HTTPStatus.INTERNAL_SERVER_ERROR
-    assert response.json == 'Cannot revoke token.'
+    assert response.json == "Cannot revoke token."
 
 
-@mock_backend_response({'remote': {'revoke': {'result': 1234}}})
+@mock_backend_response({"remote": {"revoke": {"result": 1234}}})
 def test_delete_client_unexpected_result(client):
     response = client.delete(EXAMPLE_TOKEN_URL)
     assert response.status_code == HTTPStatus.INTERNAL_SERVER_ERROR
-    assert response.json == 'Cannot revoke token.'
+    assert response.json == "Cannot revoke token."
